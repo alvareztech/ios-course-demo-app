89df5768b5b5c16b956f539e60da98e1cb77b4f0
Daniel Alvarez <a3dany@gmail.com>
2017-09-24 21:39:20 -0400

    Initial Commit

---
+++ Course Demo App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Course Demo App
+//
+//  Created by Daniel Alvarez on 9/24/17.
+//  Copyright © 2017 ALVAREZ.tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
