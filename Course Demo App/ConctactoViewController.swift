6c5a6ca8c809eca4945da1139adc237f6018caf8
Daniel Alvarez <a3dany@gmail.com>
2017-09-25 22:09:01 -0400

    Create Contact static tableview

--- Course Demo App/ConctactoViewController.swift
+++ Course Demo App/ConctactoViewController.swift
@@ -1,27 +1,43 @@
 import UIKit
 
-class ConctactoViewController: UIViewController {
-
+class ConctactoViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("Index path: Section: \(indexPath.section) Row: \(indexPath.row)")
+        
+        if indexPath.section == 0 {
+            
+            if indexPath.row == 0 {
+                
+                
+                
+            } else if indexPath.row == 1 {
+                
+            } else if indexPath.row == 2 {
+                
+            }
+            
+        } else if indexPath.section == 1 {
+            
+            if indexPath.row == 0 {
+                
+            } else if indexPath.row == 1 {
+                
+            }
+            
+        }
     }
-    */
-
+    
 }
