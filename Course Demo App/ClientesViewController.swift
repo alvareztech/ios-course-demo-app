f564f4e7d7c333c1cad09da64b7b6b82e6f17fe3
Daniel Alvarez <a3dany@gmail.com>
2017-09-28 00:41:29 -0400

    Edit ClientesV

--- Course Demo App/ClientesViewController.swift
+++ Course Demo App/ClientesViewController.swift
@@ -1,35 +1,53 @@
-//
-//  ClientesViewController.swift
-//  Course Demo App
-//
-//  Created by Daniel Alvarez on 9/26/17.
-//  Copyright © 2017 ALVAREZ.tech. All rights reserved.
-//
-
 import UIKit
 
-class ClientesViewController: UIViewController {
-
+class ClientesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var clientes: [Persona] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        let persona1 = Persona(nombre: "Maria", ci: 6112231, teléfono: 77665544, correo: "maria@empresa.com")
+        let persona2 = Persona(nombre: "Julio", ci: 31263177, teléfono: 66229944, correo: "julio@empresa.com")
+        let persona3 = Persona(nombre: "Katherine", ci: 4123121, teléfono: 77221100, correo: "katherine@empresa.com")
+        
+        clientes.append(persona1)
+        clientes.append(persona2)
+        clientes.append(persona3)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Determina el número de celdas que tendra por sección, en este caso solo una sección
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clientes.count;
     }
-    */
-
+    
+    // Retorna la celda con los datos colocados por posición
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "clienteCell", for: indexPath)
+        
+        let persona: Persona = clientes[indexPath.row]
+        
+        let nombreLabel : UILabel = cell.viewWithTag(1) as! UILabel
+        let ciLabel : UILabel = cell.viewWithTag(2) as! UILabel
+        
+        nombreLabel.text = persona.nombre
+        ciLabel.text = String(persona.ci)
+        
+        return cell
+    }
+    
 }
