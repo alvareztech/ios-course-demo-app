4608b8c42aab9dff4b96681490c77f26ee9b66f5
Daniel Alvarez <a3dany@gmail.com>
2017-09-28 00:39:18 -0400

    Add clientsVC

---
+++ Course Demo App/ClientesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ClientesViewController.swift
+//  Course Demo App
+//
+//  Created by Daniel Alvarez on 9/26/17.
+//  Copyright © 2017 ALVAREZ.tech. All rights reserved.
+//
+
+import UIKit
+
+class ClientesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
